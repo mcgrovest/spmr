66b22b2ed033eca17f9f6f708a999634c56da8c0
mcgrovest <mcgrovest69@gmail.com>
2018-12-10 02:29:15 +0300

    Merge conflicts ¯\_(ツ)_/¯

--- main.py
+++ main.py
@@ -134,12 +134,6 @@ def karusel(phone):
                                      json={"phone": krslphn})
     print(response_karusel, response_karusel.content)
 
-def karusel(phone):
-    krslphn = '7'+phone
-    response_karusel = requests.post(url='https://app.karusel.ru/api/v1/phone/',
-                                     json={"phone": krslphn})
-    print(response_karusel, response_karusel.content)
-
 
 def generate_email():
     """
